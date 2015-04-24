87b8881258f17b1645f2a9917858a5d9e67dcb73
ernest1337 <e.khan@ct-college.com.ua>
2015-04-24 18:20:25 +0300

    Perehod posle zapolnenia info

--- action.php
+++ action.php
@@ -1,8 +1,33 @@
-<?php
-    echo $_POST["fio"] . " ". $_POST["fak"] . " " . $_POST["spec"];
-/**
- * Created by PhpStorm.
- * User: ernest
- * Date: 24.04.15
- * Time: 16:32
- */
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="/public/css/bootstrap.css">
+        <link rel="stylesheet" href="/public/css/style.css">
+        <title>Info Page</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="panel panel-default">
+                <!-- Default panel contents -->
+                <div class="panel-heading"> <?php
+                    echo "ФИО: " . $_POST[fio];
+                    ?>
+                </div>
+
+                <!-- List group -->
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <?php
+                            echo "Факультет: " . $_POST[fak];
+                        ?>
+                    </li>
+                    <li class="list-group-item">
+                        <?php
+                        echo "Специальность: " . $_POST[spec];
+                        ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
