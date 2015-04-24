c87abffd91a3e65159c83fbcd22b34ba5fae328f
ernest1337 <e.khan@ct-college.com.ua>
2015-04-24 15:25:16 +0300

    2 lesson/ html css

---
+++ test.php
@@ -0,0 +1,21 @@
+<html><?php
+
+    for($i=0; $i<=100; $i++){
+    if ($i%2 == 0){
+        echo $i;
+        $j++;
+        if($j<5) {
+            echo "  ";
+
+        }
+        else{
+            echo  "<br />";
+            $j=0;
+        }
+        }
+    }
+    $hello="world";
+    $world="hello";
+    echo $world . "  " . $$world;
+?>
+</html>
