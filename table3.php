251fb63cc4aeb37c20ac77410bd1c6e2c4003e92
ernest1337 <e.khan@ct-college.com.ua>
2015-04-28 17:01:09 +0000

    tablica3, diagonal, shahmatno

---
+++ table3.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/public/css/bootstrap.css">
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="/public/css/table1.css">
+    <script src="/public/js/bootstrap.js"></script>
+    <title>TABLE 2</title>
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="/24april.html"><img src="http://gloria-project.eu/wp-content/uploads/2012/06/logo-256-transp.png"></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="/24april.html"><span class="glyphicon glyphicon-header" aria-hidden="true"></span> Home</a></li>
+                <li><a><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Info</a></li>
+                <li><a href="table.php"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span> Table</a></li>
+                <li><a href="table2.php"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span> Table 2</a></li>
+                <li class="active"><a href="forma.html"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span> Form</a></li>
+            </ul>
+            <!--<ul class="nav navbar-nav navbar-right">-->
+            <!--<li class="active"><a>Info</a></li>-->
+            <!--</ul>-->
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+    <?php
+    echo '<table class="table table-bordered">';
+    for($stolb1=1; $stolb1<=$_POST['stolb']; $stolb1++){
+        echo '<tr>';
+            for ($strok1 = 1; $strok1 <= $_POST['strok']; $strok1++) {
+                $rez = $stolb1 * $strok1;
+                if($_POST['diag']=='option1') {
+                    if ($_POST['col'] == 'Coral') {
+                        $color = ($strok1 == $stolb1) ? 'style="background-color: coral;"' : '';
+                    }
+                    if ($_POST['col'] == 'Crimson') {
+                        $color = ($strok1 == $stolb1) ? 'style="background-color: crimson"' : '';
+                    }
+                    if ($_POST['col'] == 'Blue') {
+                        $color = ($strok1 == $stolb1) ? 'style="background-color: #337ab7;"' : '';
+                    }
+                    if ($_POST['col'] == 'Green') {
+                        $color = ($strok1 == $stolb1) ? 'style="background-color: greenyellow;"' : '';
+                    }
+                }
+
+                if($_POST['diag']=='option2'){
+                    if((($strok1%2)==($stolb1%2))){
+                        if ($_POST['col'] == 'Green') {
+                            $color = 'style="background-color: greenyellow;"';
+                        }
+                        if ($_POST['col'] == 'Coral') {
+                            $color = 'style="background-color: coral;"';
+                        }
+                        if ($_POST['col'] == 'Crimson') {
+                            $color = 'style="background-color: crimson;"';
+                        }
+                        if ($_POST['col'] == 'Blue') {
+                            $color = 'style="background-color: #337ab7;"';
+                        }
+
+                    }
+                    else{
+                        $color='';
+                    }
+                }
+                echo "<td " . $color . ">" . $rez . ' ' . ' </td>';
+
+            }
+        echo '<br />';
+        echo '</tr>';
+    }
+    echo '</table>';
+    if($_POST[nazv]==true) {
+        echo "Таблица умножения размером " . $_POST[stolb] . " на " . $_POST[strok];
+    }
+    ?>
+
+
+
+
+</div>
+<!--</content>-->
+<footer>
+    <a href="http://vk.com/ernieisonfire"><center>Copyrights reserved ©</center></a>
+</footer>
+
+</body>
+</html>
