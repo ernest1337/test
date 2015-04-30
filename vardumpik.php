6786a2a237d2208ef49b5165d4361d12df227813
ernest1337 <e.khan@ct-college.com.ua>
2015-04-30 13:20:49 +0000

    vardump 2.0

--- vardumpik.php
+++ vardumpik.php
@@ -24,19 +24,7 @@
     //WAY 1
         function vd($array){
             echo "<pre>";
-//            foreach($array as $key){
-//                echo gettype($key) . " (";
-//                if (gettype($key)=='array'){
-//                    echo "<br>";
-//                    return VD($key);//calling the function inside the function
-//                }
-//                else {
-//                    echo $key.")    " . "<br>";
-//                }
-//            }
-//         }
-            //END OF WAY 1
-            //WAY 2
+//
             echo gettype($array) . "(" . count($array) . ") {". "<br>";
             foreach($array as $key =>$value)
             {
